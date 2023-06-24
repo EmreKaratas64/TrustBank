dbfd5359822f3ca8ba54917f23ea65db4f2eff9c
EmreKaratas64 <emrekaratas076@gmail.com>
2023-06-24 10:19:03 +0300

    Created layers, packages downloaded

---
+++ TrustDataAccessLayer/Concrete/Context.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using TrustBank_EntityLayer.Concrete;
+
+namespace TrustBank_DataAccessLayer.Concrete
+{
+    public class Context : IdentityDbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("server=DESKTOP-NKLMS7G\\MSSQLSERVER01;initial catalog=TrustBankDB;integrated Security=true");
+        }
+
+        public DbSet<CustomerAccount> CustomerAccounts { get; set; }
+
+        public DbSet<CustomerAccountActivity> CustomerAccountsActivities { get; set; }
+    }
+}
---
+++ TrustEntityLayer/Concrete/CustomerAccountActivity.cs
@@ -0,0 +1,11 @@
+﻿namespace TrustBank_EntityLayer.Concrete
+{
+    public class CustomerAccountActivity
+    {
+        public int CustomerAccountActivityID { get; set; }
+        public string ActivityType { get; set; }
+        public decimal Amount { get; set; }
+        public DateTime ActivityDate { get; set; }
+        public string ActivityDescription { get; set; }
+    }
+}
