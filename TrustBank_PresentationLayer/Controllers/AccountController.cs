3ed8d246d000798d90ea0e4ba8fb228c99c1d7ed
EmreKaratas64 <emrekaratas076@gmail.com>
2023-06-29 17:14:40 +0300

    register form done, register controller method is not completed yet. Css fix in register form

---
+++ TrustBank_PresentationLayer/Controllers/AccountController.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using TrustBank_DTOLayer.DTOs.AppUserDTOs;
+using TrustBank_EntityLayer.Concrete;
+
+namespace TrustBank_PresentationLayer.Controllers
+{
+    public class AccountController : Controller
+    {
+        private readonly UserManager<AppUser> _userManager;
+
+        public AccountController(UserManager<AppUser> userManager)
+        {
+            _userManager = userManager;
+        }
+
+        [HttpGet]
+        public IActionResult Register()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Register(AppUserRegisterDto appUserRegisterDto)
+        {
+            if (ModelState.IsValid)
+            {
+                AppUser appUser = new AppUser()
+                {
+                    Name = appUserRegisterDto.Name,
+                    Surname = appUserRegisterDto.Surname,
+                    Email = appUserRegisterDto.Email,
+                    UserName = appUserRegisterDto.Username
+                };
+                var result = await _userManager.CreateAsync(appUser, appUserRegisterDto.Password);
+                if (result.Succeeded)
+                {
+                    return RedirectToAction("MailVerify", "Account");
+                }
+            }
+            return View(appUserRegisterDto);
+        }
+    }
+}
--- Trust_DTOLayer/DTOs/AppUserDTOs/AppUserRegisterDto.cs
+++ Trust_DTOLayer/DTOs/AppUserDTOs/AppUserRegisterDto.cs
@@ -3,7 +3,6 @@
     public class AppUserRegisterDto
     {
         public string Name { get; set; }
-
         public string Surname { get; set; }
         public string Email { get; set; }
         public string Username { get; set; }
