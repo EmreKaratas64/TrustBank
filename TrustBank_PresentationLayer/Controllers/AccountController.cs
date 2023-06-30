226ecee16574df62fc95e251925441e7f3c73adc
EmreKaratas64 <emrekaratas076@gmail.com>
2023-06-30 12:21:46 +0300

    register popup remove, identity error override, email confirm code creation

--- TrustBank_PresentationLayer/Controllers/AccountController.cs
+++ TrustBank_PresentationLayer/Controllers/AccountController.cs
@@ -25,17 +25,29 @@ namespace TrustBank_PresentationLayer.Controllers
         {
             if (ModelState.IsValid)
             {
+                Random random = new Random();
                 AppUser appUser = new AppUser()
                 {
                     Name = appUserRegisterDto.Name,
                     Surname = appUserRegisterDto.Surname,
                     Email = appUserRegisterDto.Email,
-                    UserName = appUserRegisterDto.Username
+                    UserName = appUserRegisterDto.Username,
+                    City = "aaa",
+                    District = "bbbb",
+                    ImgUrl = "ccc",
+                    ConfirmCode = random.Next(100000, 1000000)
                 };
                 var result = await _userManager.CreateAsync(appUser, appUserRegisterDto.Password);
                 if (result.Succeeded)
                 {
                     return RedirectToAction("MailVerify", "Account");
+                }
+                else
+                {
+                    foreach (var item in result.Errors)
+                    {
+                        ModelState.AddModelError("", item.Description);
+                    }
                 }
             }
             return View(appUserRegisterDto);
