480a016528d106fab349cf1642cefdc2caf84f85
EmreKaratas64 <emrekaratas076@gmail.com>
2023-06-30 21:28:06 +0300

    mail send done, confirmation form made, mail confirmed updated

--- TrustBank_PresentationLayer/Controllers/AccountController.cs
+++ TrustBank_PresentationLayer/Controllers/AccountController.cs
@@ -1,7 +1,10 @@
-﻿using Microsoft.AspNetCore.Identity;
+﻿using MailKit.Net.Smtp;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using MimeKit;
 using TrustBank_DTOLayer.DTOs.AppUserDTOs;
 using TrustBank_EntityLayer.Concrete;
+using TrustBank_PresentationLayer.Models;
 
 namespace TrustBank_PresentationLayer.Controllers
 {
@@ -40,7 +43,26 @@ namespace TrustBank_PresentationLayer.Controllers
                 var result = await _userManager.CreateAsync(appUser, appUserRegisterDto.Password);
                 if (result.Succeeded)
                 {
-                    return RedirectToAction("MailVerify", "Account");
+                    MimeMessage mimeMessage = new MimeMessage();
+                    MailboxAddress mailboxAddressFrom = new MailboxAddress("TrustBank Admin", "coreblog@ilkportfolio.com");
+                    MailboxAddress mailboxAddressTo = new MailboxAddress("User", appUser.Email);
+
+                    mimeMessage.From.Add(mailboxAddressFrom);
+                    mimeMessage.To.Add(mailboxAddressTo);
+
+                    var bodyBuilder = new BodyBuilder();
+
+                    bodyBuilder.TextBody = "Hesabınız için doğrulama kodu: " + appUser.ConfirmCode;
+                    mimeMessage.Body = bodyBuilder.ToMessageBody();
+                    mimeMessage.Subject = "TrustBank Hesap Onay Kodu";
+
+                    SmtpClient client = new SmtpClient();
+                    client.Connect("webmail.ilkportfolio.com", 587, false);
+                    client.Authenticate("coreblog@ilkportfolio.com", "cr798*BK");
+                    client.Send(mimeMessage);
+                    client.Disconnect(true);
+                    TempData["Mail"] = appUser.Email;
+                    return RedirectToAction("MailVerify");
                 }
                 else
                 {
@@ -51,6 +73,32 @@ namespace TrustBank_PresentationLayer.Controllers
                 }
             }
             return View(appUserRegisterDto);
+        }
+
+        [HttpGet]
+        public IActionResult MailVerify()
+        {
+            ViewBag.mail = TempData["Mail"];
+            return View();
+        }
+
+
+        [HttpPost]
+        public async Task<IActionResult> MailVerify(ConfirmMail confirmMail)
+        {
+            if (ModelState.IsValid)
+            {
+                var user = await _userManager.FindByEmailAsync(confirmMail.Mail);
+                if (user == null) return NotFound();
+                if (user.ConfirmCode.ToString() == confirmMail.ConfirmCode)
+                {
+                    user.EmailConfirmed = true;
+                    await _userManager.UpdateAsync(user);
+                    return RedirectToAction("Dashboard", "User");
+                }
+            }
+
+            return View(confirmMail);
         }
     }
 }
---
+++ TrustBank_PresentationLayer/Models/ConfirmMail.cs
@@ -0,0 +1,14 @@
+﻿
+using System.ComponentModel.DataAnnotations;
+
+namespace TrustBank_PresentationLayer.Models
+{
+    public class ConfirmMail
+    {
+        [Required(ErrorMessage = "Mail adresi boş olamaz")]
+        [EmailAddress(ErrorMessage = "Mail adresi geçersiz")]
+        public string Mail { get; set; }
+
+        public string ConfirmCode { get; set; }
+    }
+}
